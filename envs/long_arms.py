f4ff64b457b2c4ce002353b20fac941423fc7a97
im-ant <anthony.gx.chen@gmail.com>
2020-07-01 11:12:53 -0400

    wrote and tested the basic state transition script with image observations, should be working

--- envs/long_arms.py
+++ envs/long_arms.py
@@ -10,13 +10,15 @@
 # Author: Anthony G. Chen
 # ============================================================================
 
-
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
+import numpy as np
+
 from torch.utils.data import DataLoader, Subset
 from torchvision.datasets import CIFAR10
+from torchvision import transforms
 
 
 class LongArmsEnv(gym.Env):
@@ -40,41 +42,49 @@ class LongArmsEnv(gym.Env):
 
     """
 
-    def __init__(self, dataset_path='./cifar_data/'):
+    def __init__(self, corridor_length=5,
+                 img_size=(32, 32), grayscale=True, flatten_obs=True,
+                 dataset_path='./cifar_data_tmp/'):
 
-        # State = (arm #, arm steps)
-        self.corridor_length = None  # TODO set number
+        # ==
+        # Attributes
+        self.corridor_length = corridor_length
         self.num_arms = 2
 
+        self.img_size = img_size
+        self.grayscale = grayscale
+        self.flatten_obs = flatten_obs
+
         # ==
+        # Initialize spaces
         self.action_space = spaces.Discrete(2)
-        self.observation_space = None # TODO depends on image
+        self.observation_space = None  # TODO depends on image
 
         # ==
-        # Download dataset
+        # Get dataset of images
+        dataset_tup = self._init_img_dataset(dataset_path)
+        self.img_dict = dataset_tup[0]
+        self.corridor_ds = dataset_tup[1]
 
+        # ==
+        # Initialize
+        # State = (arm #, arm steps)
+        self.state = (0, 0)
 
-        tmp = self._init_observations(dataset_path)
-
-        self.state = (0,0)
-
-        pass
-
-    def _init_observations(self, dataset_path):
+    def _init_img_dataset(self, dataset_path):
         """
         Initialize image dataset corresponding to each observaiton
 
-
-
-        Return: dict containing the initial, final and each arm's entrance
-                image observation
-                    {'init': PIL.Image.Image,
+        :param dataset_path:
+        :return: dict containing the initial, final and each arm's entrance
+                 image observation
+                    {'initial': PIL.Image.Image,
                      '1': PIL.Image.Image,
                      '2': PIL.Image.Image,
                      ...,
                      'final': PIL.Image.Image}
-
-                sampler
+                 torch.utils.data.dataset.Subset object of corridor images
+                 to be sampled. Each entry contains (PIL.Image.Image, label)
         """
 
         # Define the image classes of each state
@@ -115,43 +125,64 @@ class LongArmsEnv(gym.Env):
         final_sample = ds[final_idxs[64]]
         arm_samples = []
         for arm_j in range(self.num_arms):
-            cur_arm_sample = ds[arm_idxs[2*arm_j]]
+            cur_arm_sample = ds[arm_idxs[2 * arm_j]]
             arm_samples.append(cur_arm_sample)
 
         # Save samples as PIL images
         img_obs_dict = {
-            'init': init_sample[0],
+            'initial': init_sample[0],
             'final': final_sample[0],
         }
         for arm_j in range(self.num_arms):
-            img_obs_dict[str((arm_j+1))] = arm_samples[arm_j][0]
+            img_obs_dict[str((arm_j + 1))] = arm_samples[arm_j][0]
 
         # ==
         # Set up the image sampler for the corridor
+        corridor_ds = Subset(ds, corri_idxs)
 
-        # TODO BELOW IS ALL WRONG CODE
-        # Figure out when we have indeces and a original dataset (ds), how do
-        # we get a simple way to sample data from it
+        return img_obs_dict, corridor_ds
 
-        #corridor_ds = Subset(ds, corri_idxs)
-        corridor_ds = ds.data[corri_idxs]
-        data_loader = DataLoader(corridor_ds, batch_size=1, shuffle=True,
-                                 num_workers=0)
+    def _init_obs_space(self):
+        """
+        Initialize the observation space
+        :return: gym.spaces observation space
+        """
 
+        # TODO THIS
 
+        pass
 
-        print(img_obs_dict)
-        print(corridor_ds)
-        #print(data_loader)
+    def _process_img(self, img):
+        """
+        Transform an PIL image to the correct observation space
+        :param img: a single PIL.Image.Image object
+        :return: numpy array of transformed image
+        """
 
-        tmp = next(corridor_ds)
-        print(tmp)
+        # ==
+        # Construct transforms
+        trans_list = [transforms.Resize(self.img_size)]
+        if self.grayscale:
+            trans_list += [transforms.Grayscale(num_output_channels=1)]
 
+        img_transforms = transforms.Compose(trans_list)
 
+        # ==
+        # Transform and output
+        img = img_transforms(img)
 
+        img = np.array(img)
+        if self.flatten_obs:
+            img = img.flatten()
 
+        return img
 
     def step(self, action):
+        """
+
+        :param action:
+        :return:
+        """
 
         # TODO assert action range?
 
@@ -160,27 +191,58 @@ class LongArmsEnv(gym.Env):
         cur_arm, cur_step = self.state
         if cur_arm == 0:
             self.state = ((action + 1), 1)
+        elif cur_step >= (self.corridor_length + 3):
+            pass
         else:
             self.state = (cur_arm, cur_step + 1)
 
         # ==
-        # Generate observation
+        # Generate observation, reward and done
+        obs = None
+        reward = 0.0
+        done = False
 
+        # Get boolean
+        is_after_corri = (self.state[1] == (self.corridor_length + 2))
+        is_final_state = (self.state[1] >= (self.corridor_length + 3))
 
-        # ==
-        # Generate reward, done and info
+        # If in the first arm states
+        # (assume agent can never "step" to the starting state)
+        if self.state[1] == 1:
+            img = self.img_dict[str(self.state[0])]
 
-        # TODO
+        # If in the state after corridor
+        elif is_after_corri:
+            img = self.img_dict['final']
 
+        # If in the terminal states
+        elif is_final_state:
+            img = self.img_dict['final']
+            done = True
+            if self.state[0] == 1:
+                reward = 1.0
+            elif self.state[0] == 2:
+                reward = -1.0
+
+        # If in the corridor states
+        else:
+            img = next(iter(self.corridor_ds))[0]
+
+        # Process image
+        obs = self._process_img(img)
+
+        return obs, reward, done, {}
 
     def reset(self):
-        self.state = ((action + 1), 1)
+        # Reset state
+        self.state = (0, 0)
 
         # ==
         # Generate observation
+        init_img = self.img_dict['initial']
+        init_obs = self._process_img(init_img)
 
-
-        pass
+        return init_obs
 
     def render(self, mode='human'):
         pass
@@ -195,3 +257,15 @@ if __name__ == '__main__':
 
     env = LongArmsEnv()
     print(env)
+
+    obs = env.reset()
+    print(env.state, np.shape(obs), '[', np.min(obs), np.max(obs), ']')
+
+    for step in range(10):
+
+        obs, reward, done, info = env.step(0)
+
+        print(env.state, np.shape(obs), '[', np.min(obs), np.max(obs), ']', reward, done)
+
+
+
