b0e0632b2ee1eb431ef0b546da7aad3e18b33924
im-ant <anthony.gx.chen@gmail.com>
2020-07-25 13:56:38 -0400

    working on the mila cluster now

---
+++ long_arms/submit_multi_jobs.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# ============================================================================
+# Bash script to submit multiple jobs
+
+
+# Optional: message to put into the created directory? TODO fix this
+dir_message="Testing long arm with recurrent DQN"
+
+# The parent experimental directory, each experiment will be stored as a sub-
+# directory within this directory
+dir_path="/network/tmp1/chenant/ant/cred_transport/long_arms/07-25/exp1_test"
+
+# Path to the parent directory containing the configuration files, one per
+# experiment. Experiment folder will be named after the config file names
+exp_config_dir_path="$dir_path/config"
+
+# Job file path
+job_file="/home/mila/c/chenant/repos/credit-transport/long_arms/arg-job_train-longarm.script"
+
+# Job partition (same for all jobs)
+partition_per_job="main,long"
+
+# Job resource (same for all jobs)
+gres_per_job="gpu:1"
+
+# Specify cpu need (same for all jobs)
+cpu_per_task="1"
+
+# Specify memory (RAM) need (same for all jobs)
+mem_per_job="12G"
+
+# Specify time need (same for all jobs)
+time_per_job="12:00:00"
+
+
+
+
+# ============================================================================
+# Below is automatically ran
+
+# ==
+# Little print-out
+echo "Job submitted: `date`" > "$dir_path/submit_note.txt"
+echo $dir_message >> "$dir_path/submit_note.txt"
+
+# ==
+# Iterate over each configuration file
+for config_file in $exp_config_dir_path/*.ini ; do
+  # Get the config name without parent path or file suffix
+  config_name=$(basename --suffix='.ini' "$config_file")
+  # Get exp name via splitting config_name by "_" and taking the
+  # first sub-string. NOTE: group run by the string before first "_"
+  exp_name="$( cut -d '_' -f 1 <<< "$config_name" )"
+  # This is the "parent" file that should have the same setting but
+  # contain runs of different seeds
+  exp_path="$dir_path/$exp_name"
+
+  # Create exp directory
+  echo "Experiment directory path: $exp_path"
+  if [ ! -d "$exp_path" ] ; then
+    mkdir -p $exp_path
+  fi
+
+  # Create error and output files
+  cur_error_file="$exp_path/error_$exp_name.txt"
+  cur_out_file="$exp_path/out_$exp_name.txt"
+
+  # ==
+  # Submit job
+  sbatch --cpus-per-task=$cpu_per_task \
+         --partition=$partition_per_job \
+         --gres=$gres_per_job \
+         --mem=$mem_per_job \
+         --time=$time_per_job \
+         --output="$cur_out_file" \
+         --error="$cur_error_file" \
+         --export=logpath="$exp_path",configpath="$config_file" \
+         --job-name=$config_name \
+         $job_file
+
+done
+
+
+
--- long_arms/train_rlpyt.py
+++ long_arms/train_rlpyt.py
@@ -12,17 +12,24 @@
 #   algot: rlpyt/algos/dqn/cat_dqn.py
 ##############################################################################
 
+import argparse
+import configparser
+import math
+import random  # only imported to set seed
+import sys
+
+import gym
+import numpy as np
+import torch
+
 from rlpyt.samplers.serial.sampler import SerialSampler
 from rlpyt.samplers.collections import TrajInfo
 from rlpyt.samplers.parallel.cpu.collectors import CpuWaitResetCollector
 from rlpyt.envs.gym import GymEnvWrapper
 from rlpyt.envs.atari.atari_env import AtariEnv, AtariTrajInfo
-from rlpyt.algos.dqn.cat_dqn import CategoricalDQN
-# from rlpyt.algos.dqn.r2d1 import R2D1
-from rlpyt.agents.dqn.catdqn_agent import CatDqnAgent
 from rlpyt.agents.dqn.r2d1_agent import R2d1Agent
-from rlpyt.models.dqn.atari_catdqn_model import AtariCatDqnModel
 from rlpyt.models.dqn.atari_r2d1_model import AtariR2d1Model
+from rlpyt.models.dqn.atari_dqn_model import AtariDqnModel
 from rlpyt.runners.minibatch_rl import MinibatchRl, MinibatchRlEval
 from rlpyt.utils.logging.context import logger_context
 
@@ -34,78 +41,168 @@ def env_f(**kwargs):
     return GymEnvWrapper(LongArmsEnv(**kwargs))
 
 
-def build_and_train(run_ID=0, cuda_idx=None, n_parallel=2):
+def build_and_train(config: configparser.ConfigParser,
+                    cuda_idx: int = None,
+                    n_parallel: int = 1,
+                    log_dir: str = None):
+    # =====
+    # Set up hardware
     affinity = dict(cuda_idx=cuda_idx, workers_cpus=list(range(n_parallel)))
     gpu_cpu = "CPU" if cuda_idx is None else f"GPU {cuda_idx}"
     print(f"Using serial sampler, {gpu_cpu} for sampling and optimizing.")
 
-    # ==
-    # Env
-    env_args = {'corridor_length': 5,
-                'img_size': (20, 20),
-                'grayscale': True,
-                'flatten_obs': False,
-                'scale_observation': False,
-                'dataset_path': '/network/tmp1/chenant/ant/dataset/cifar/'}
+    # =====
+    # Set up environment
+    img_len = config['Env'].getint('img_len')  # side length of the cifar img
+    corridor_len = config['Env'].getint('corridor_length')
+    env_args = {
+        'corridor_length': corridor_len,
+        'img_size': (img_len, img_len),
+        'grayscale': config['Env'].getboolean('grayscale'),
+        'flatten_obs': config['Env'].getboolean('flatten_obs'),
+        'scale_observation': config['Env'].getboolean('scale_observation'),
+        'dataset_path': config['Env']['dataset_path']
+    }
 
-    # ==
-    # Sampler
+    # =====
+    # Set up algorithm
+    algo_kwargs = {
+        'discount': config['Algorithm'].getfloat('discount'),
+        'lambda_coef': config['Algorithm'].getfloat('lambda_coef'),
+        'use_recurrence': config['Algorithm'].getboolean('use_recurrence'),
+        'target_update_interval': int(config['Algorithm'].getfloat('target_update_interval')),
+        'min_steps_learn': int(config['Algorithm'].getfloat('min_steps_learn')),
+        'eps_steps': int(config['Algorithm'].getfloat('eps_steps')),
+        'double_dqn': config['Algorithm'].getboolean('double_dqn'),
+        'learning_rate': config['Algorithm'].getfloat('learning_rate'),
+        'clip_grad_norm': config['Algorithm'].getfloat('clip_grad_norm'),
+        'delta_clip': None,  # default is squared error
+        'OptimCls': torch.optim.Adam,
+        'optim_kwargs': None,
+        'initial_optim_state_dict': None,
+    }
+
+    # =====
+    # Set up model
+    img_channels = 1 if config['Env'].getboolean('grayscale') else 3
+    model_kwargs = {
+        'image_shape': (img_channels, img_len, img_len),
+        'output_size': config['Env'].getint('num_arms'),
+        'fc_size': config['Model'].getint('fc_size'),
+        'lstm_size': config['Model'].getint('lstm_size'),
+        'head_size': config['Model'].getint('head_size'),
+        'dueling': config['Model'].getboolean('dueling'),
+    }
+
+    # =====
+    # Set up logger
+    # TODO set up the experiment name properly
+    logger_kwargs = {
+        'log_dir': log_dir,
+        'name': config['Training']['exp_name'],
+        'run_ID': config['Training'].getint('seed'),
+        'snapshot_mode': config['Training']['log_snapshot_mode'],
+    }
+
+    # ==========
+    # Initialize environment sampler
     sampler = SerialSampler(
         EnvCls=env_f,
-        TrajInfoCls=TrajInfo,  # default traj info + GameScore
-        CollectorCls=CpuWaitResetCollector,  # don't immediate reset episode
+        TrajInfoCls=TrajInfo,  # collect default trajectory info
+        CollectorCls=CpuWaitResetCollector,  # each batch only has at most 1 episode
         env_kwargs=env_args,
-        eval_env_kwargs=env_args,
-        batch_T=10,  # seq length of per batch of sampled data
+        batch_T=(corridor_len + 3 + 2),  # seq length of per batch of sampled data
         batch_B=1,
         max_decorrelation_steps=0,
+        eval_env_kwargs=env_args,  # eval stuff, don't think it is used
         eval_n_envs=0,
         eval_max_steps=int(10e3),
         eval_max_trajectories=5,
     )
 
-    # ==
-    # Algorithm and agent
-
-    # Configure in:
-    # rlpyt/models/dqn/atari_catdqn_model.py
-    model_kwargs = {'image_shape': (1, 20, 20),
-                    'output_size': 2}
-    algo_kwargs = {'batch_T': 3, 'batch_B': 2, 'warmup_T': 1,
-                   'store_rnn_state_interval': 4,
-                   'min_steps_learn': 16,
-                   'replay_size': 10, 'replay_ratio': 1,
-                   'target_update_interval': 1, 'n_step_return': 2,
-                   'learning_rate': 0.0001, 'eps_steps': 10000,
-                   'double_dqn': False, 'prioritized_replay': False,
-                   'input_priorities': False, 'updates_per_sync': 1}
+    # ==========
+    # Initialize algorithm, agent and model
     algo = R0D1(**algo_kwargs)
     agent = R2d1Agent(ModelCls=AtariR2d1Model,
                       model_kwargs=model_kwargs)
 
-
-    # ==
-    # Runner
-    # can also use MinibatchRlEval
+    # ==========
+    # Initialize runner
+    # (note can use MinibatchRlEval to also have eval runs)
     runner = MinibatchRl(
         algo=algo,
         agent=agent,
         sampler=sampler,
-        n_steps=(50e6),  # total steps to train I think
-        log_interval_steps=1000,
+        n_steps=int(config['Training'].getfloat('total_num_steps')),
+        log_interval_steps=int(config['Training'].getfloat('log_interval_steps')),
         affinity=affinity,
+        seed=config['Training'].getint('seed'),
     )
-    config = dict(game='LongArms')
-    name = "a2c_" + 'LongArms'
-    log_dir = "example_3"
 
-    with logger_context(log_dir, run_ID, name, config):
+    # ==========
+    # Initialize logger and run experiment
+    with logger_context(**logger_kwargs):
         runner.train()
 
 
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    # =====================================================
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    # Configuration file
+    parser.add_argument(
+        '--config_path', type=str,
+        default='/home/mila/c/chenant/repos/credit-transport/long_arms/default_config.ini',
+        help='path to the agent configuration .ini file'
+    )
+    # Logger parent path
+    parser.add_argument(
+        '--log_dir', type=str, default='./tmp_log',
+        help='file path to the log file parent dir (default: ./tmp_log)'
+    )
+
+    # ======
+    # Parse arguments
+    args = parser.parse_args()
+    print(args)
+
+    # ======
+    # Parse config
+    config = configparser.ConfigParser()
+    config.read(args.config_path)
+
+    # =====================================================
+    # Initialize GPU
+    cuda_idx = 0 if torch.cuda.is_available() else None
+    print('cuda_idx:', cuda_idx)
+
+    # =====================================================
+    # Set seeds
+    seed = config['Training'].getint('seed')
+    torch.cuda.manual_seed_all(seed)
+    torch.random.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    # =====================================================
+    # Run experiment
+    build_and_train(
+        config=config,
+        cuda_idx=cuda_idx,
+        n_parallel=1,
+        log_dir=args.log_dir,
+    )
+
+
+
+    """
+    
+    usual dir:
+    log_dir = "/home/mila/c/chenant/repos/credit-transport/long_arms/tmp_log"
+
     parser.add_argument('--run_ID', help='run identifier (logging)', type=int, default=0)
     parser.add_argument('--cuda_idx', help='gpu to use ', type=int, default=None)
     parser.add_argument('--n_parallel', help='number of sampler workers', type=int, default=1)
@@ -115,3 +212,4 @@ if __name__ == "__main__":
         cuda_idx=args.cuda_idx,
         n_parallel=args.n_parallel,
     )
+    """
